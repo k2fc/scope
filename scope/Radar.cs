aa6bf583d9115e110a44e781a5ca76a1194805cd
dg <dennis.graiani@gmail.com>
2021-11-06 15:36:57 -0400

    Add altimeter correction to altitude

--- scope/Radar.cs
+++ scope/Radar.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Xml.Serialization;
 using DGScope.Receivers;
+using libmetar;
+using libmetar.Services;
 
 namespace DGScope
 {
@@ -32,7 +36,85 @@ namespace DGScope
         public List<Aircraft> Aircraft { get; } = new List<Aircraft>();
         public ListOfIReceiver Receivers { get; set; } = new ListOfIReceiver();
         public Font DataBlockFont { get; set; }
+        private List<string> _altimeterStations = new List<string>();
+        public List<string> AltimeterStations 
+        {
+            get
+            {
+                return _altimeterStations;
+            }
+            set
+            {
+                _altimeterStations = value;
+                correctioncalculated = false;
+            }
+        }
 
+        private int _altimeterCorrection = 0;
+        private MetarService metarService = new MetarService();
+        public List<Metar> AllMetars
+        {
+            get 
+            {
+                if (lastMetarUpdate < DateTime.Now.AddMinutes(-5))
+                {
+                    List<Metar> tempMetars = new List<Metar>();
+                    foreach (var metar in metarService.GetBulk())
+                    {
+                        try
+                        {
+                            metar.Parse();
+                            tempMetars.Add(metar);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Excluded {0} because: {1}", metar.Icao, ex.Message);
+                        }
+                    }
+                    lastMetarUpdate = DateTime.Now;
+                    correctioncalculated = false;
+                    allMetars = tempMetars;
+                }
+                return allMetars;
+            }
+        }
+        private List<Metar> allMetars;
+        public List<Metar> Metars
+        {
+            get
+            {
+                return AllMetars.Where(metar => AltimeterStations.Contains(metar.Icao)).ToList();
+            }
+        }
+        private bool correctioncalculated = false;
+        
+        public int AltimeterCorrection
+        {
+            get
+            {
+                if (!correctioncalculated || lastMetarUpdate < DateTime.Now.AddMinutes(-5))
+                {
+                    double totalaltimeter = 0;
+                    if (Metars.Count > 0)
+                    {
+                        foreach (var metar in Metars)
+                        {
+                            totalaltimeter += Converter.Pressure(metar.Pressure, libmetar.Enums.PressureUnit.inHG).Value;
+                        }
+                        totalaltimeter /= Metars.Count;
+                    }
+                    else
+                    {
+                        totalaltimeter = 29.92;
+                    }
+                    _altimeterCorrection = (int)((totalaltimeter - 29.92) * 1000);
+                    correctioncalculated = true;
+                }
+                return _altimeterCorrection;
+            }
+        }
+
+        DateTime lastMetarUpdate = DateTime.MinValue;
         public double LatitudeOfTarget(double distance, double bearing)
         {
             double R = 3443.92; // nautical miles
@@ -86,7 +168,6 @@ namespace DGScope
         }
         public Radar()
         {
-
         }
 
     }
