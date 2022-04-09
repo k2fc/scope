84990a0e83562eb932ee82ebb5fd4202a5861471
dg <dennis.graiani@gmail.com>
2022-04-09 09:58:37 -0400

    try a different parsing method

--- scope/Line.cs
+++ scope/Line.cs
@@ -8,13 +8,23 @@
         public double Length { get => End1.DistanceTo(End2); }
         public Line(GeoPoint End1, GeoPoint End2) { this.End1 = End1; this.End2 = End2; }
         public Line() { }
-        public static Line Parse(string line)
+        public static bool TryParse(string linestring, out Line line)
         {
-            line = line.Trim();
-            var lineparts = line.Split(' ');
+            linestring = linestring.Trim();
+            line = new Line();
+            var lineparts = linestring.Split(' ');
+            if (lineparts.Length < 4)
+                return false;
             string point1 = string.Concat(lineparts[0], " ", lineparts[1]);
             string point2 = string.Concat(lineparts[2], " ", lineparts[3]);
-            return new Line(GeoPoint.Parse(point1), GeoPoint.Parse(point2));
+            if (GeoPoint.TryParse(point1, out GeoPoint geoPoint1))
+                if (GeoPoint.TryParse(point2, out GeoPoint geoPoint2))
+                {
+                    line.End1 = geoPoint1;
+                    line.End2 = geoPoint2;
+                    return true;
+                }
+            return false;
         }
     }
 }
