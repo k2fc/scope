3ad038740d2908253e39c1fd992cefed1dc60c26
Brin Brody <825orion@gmail.com>
2022-08-02 07:20:49 -0400

    Refactoring for Usability (#4)
    * Refactoring for Usability
    
    settingsPath is now facilityConfig to make way for a future clientConfig XML to universalize the facility config files.
    
    CLI arguments now follow standard CLI convention (as defined by http://docopt.org/)
    
    Added a default to ver. 1.0.0 if no version is found... This is mostly a hack to make things run correctly locally, but could apply to future version checking
    
    * Allow /S Screensaver Call
    
    Because windows is weird and bad
    
    * Abandon sanity, return to Windows
    
    /C, /P, /S screensaver commands work again now. Also, you can open files by dragging them onto the EXE (or, via shortcut, simply passing the filepath).
    
    * Reorganize start sequence
    
    Cleaned up the startup sequence to allow for more versatility with shortcut/CLI arguments.  Screensavers can now be passed a particular config file to run, and users who fail to select a facility config file on startup will be prompted to create a new one (or not).
    
    * tweak arguments
    
    * actually allow the user to create a new config
    
    * restore writing the version number to the output
    
    * Slightly clearer fake version string
    
    Co-authored-by: dg <dennis.graiani@gmail.com>

--- scope/Program.cs
+++ scope/Program.cs
@@ -10,72 +10,129 @@ namespace DGScope
 {
     static class Program
     {
-        static void Start(bool screensaver = false, string settingsPath = null)
+        static void Start(bool screensaver = false, string facilityConfig = null)
         {
-            if (screensaver)
-                settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DGScope.xml");
-            else if (settingsPath == null)
+
+            //Only prompt the user for a facilityConfig file if they are not using screensaver mode, otherwise, open the default passed file
+            if (screensaver && facilityConfig==null)
+                facilityConfig = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DGScope.xml");
+            else if (facilityConfig == null)
             {
                 using (OpenFileDialog open = new OpenFileDialog())
                 {
-                    open.Filter = "xml files (*.xml)|*.xml|All files (*.*)|*.*";
+                    open.Filter = "Facility Config File (*.xml)|*.xml|All files (*.*)|*.*";
                     open.FilterIndex = 1;
-                    open.CheckFileExists = false;
+                    open.CheckFileExists = true;
                     if (open.ShowDialog() == DialogResult.OK)
                     {
-                        settingsPath = open.FileName;
+                        facilityConfig = open.FileName;
                     }
                     else
                     {
-                        Environment.Exit(0);
+                        var mboxresult = MessageBox.Show("Would you like to create a new config file?","No Config File Selected", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                        if (mboxresult == DialogResult.Yes)
+                        {
+                            using (SaveFileDialog save = new SaveFileDialog())
+                            {
+                                save.Filter = "Facility Config File (*.xml)|*.xml|All files (*.*)|*.*";
+                                save.FilterIndex = 1;
+                                save.CheckFileExists = false;
+                                if (save.ShowDialog() == DialogResult.OK)
+                                {
+                                    var newfile = save.FileName;
+                                    try
+                                    {
+                                        using (_ = File.Create(newfile))
+                                        facilityConfig = newfile;
+                                        File.Delete(newfile);
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        MessageBox.Show(ex.Message);
+                                    }
+                                    if (facilityConfig != null)
+                                        Start(false, facilityConfig);
+                                }
+                            }
+                        }
+                        else
+                            Environment.Exit(0);
                     }
                 }
             }
             RadarWindow radarWindow;
-            if (File.Exists(settingsPath))
+            if (File.Exists(facilityConfig))
             {
-                radarWindow = TryLoad(settingsPath);
+                radarWindow = TryLoad(facilityConfig);
             }
             else
             {
                 radarWindow = new RadarWindow();
                 if (!screensaver)
                 {
-                    MessageBox.Show("No config file found. Starting a new config.");
+                    //MessageBox.Show("No config file found. Starting a new config.");
                     PropertyForm propertyForm = new PropertyForm(radarWindow);
                     propertyForm.ShowDialog();
-                    radarWindow.SaveSettings(settingsPath);
+                    radarWindow.SaveSettings(facilityConfig);
                 }
             }
-            radarWindow.Run(screensaver, settingsPath);
+            radarWindow.Run(screensaver, facilityConfig);
         }
         [STAThread]
         static void Main(string[] args)
         {
             string gitVersion = string.Empty;
             using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DGScope.version.txt"))
+            //Don't get mad at me... This is the only way I could get it to stop throwing a "not found" error on the version file... Even though the version file exists
+            gitVersion = stream == null ? "No git version description" : new StreamReader(stream).ReadToEnd();
+            /*
+            //Here's your old code in case you decide to delete mine above
             using (StreamReader reader = new StreamReader(stream))
             {
                 gitVersion = reader.ReadToEnd();
             }
+            */
             Console.WriteLine(gitVersion);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             LoadReceiverPlugins();
             bool screensaver = false;
             bool inhibit = false;
-            string settingsPath = null;
+            string facilityConfig = null;
             if (args.Length > 0)
             {
-                foreach (var arg in args)
+                foreach (var argument in args)
                 {
-                    if (arg.Contains("/C"))
+                    String arg = argument.ToLower();
+                    //Selecting which facility config file to use:
+                    //Either take a filepath as a parameter with the file argument, or accept it as a sole argument.  If neither is a valid file path, leave as null to be handled upon Start()
+
+                    //Argument format: "--f=PATH" or "--file=PATH" (quotes need not be included if running via the command line, but must be included if passed as arguments via Visual Studio's Debug Config)
+                    if (arg.StartsWith("--f") || arg.StartsWith("--file"))
                     {
-                        settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DGScope.xml");
-                        RadarWindow radarWindow;
-                        if (File.Exists(settingsPath))
+                        string paramFileName = arg.Split('=')[1].Trim();
+                        if (File.Exists(paramFileName))
+                            facilityConfig = paramFileName;
+                    }
+                    //Allow for drag 'n drop or "open with" functionality
+                    else if (File.Exists(arg))
+                    {
+                        facilityConfig = arg;
+                    }
+
+                    //Screensaver commands:
+
+                    //Start in config mode
+                    if (arg.StartsWith("--c") || arg.StartsWith("--config") || arg.StartsWith("/c"))
+                    {
+                        if(facilityConfig == null)
                         {
-                            radarWindow = TryLoad(settingsPath);
+                            facilityConfig = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "DGScope.xml");
+                        }
+                        RadarWindow radarWindow;
+                        if (File.Exists(facilityConfig))
+                        {
+                            radarWindow = TryLoad(facilityConfig);
                         }
                         else
                         {
@@ -83,31 +140,30 @@ namespace DGScope
                         }
                         PropertyForm propertyForm = new PropertyForm(radarWindow);
                         propertyForm.ShowDialog();
-                        radarWindow.SaveSettings(settingsPath);
+                        radarWindow.SaveSettings(facilityConfig);
                         inhibit = true;
                     }
-                    if (arg.Contains("/S"))
+                    //Start screensaver in normal mode
+                    else if (arg.StartsWith("--s") || arg.StartsWith("--screensaver") || arg.StartsWith("/c"))
                     {
                         screensaver = true;
                     }
-                    else if (arg.Contains("/P"))
+                    //Start screensaver in child mode
+                    else if (arg.StartsWith("--p") || arg.StartsWith("--screensaver_child") || arg.StartsWith("/p"))
                     {
                         inhibit = true;
                     }
-                    else
-                    {
-                        settingsPath = arg;
-                    }
+                    
                 }
                 
                 if (!inhibit)
                 {
-                    Start(screensaver, settingsPath);
+                    Start(screensaver, facilityConfig);
                 }
             }
             else
             {
-                Start(false);
+                Start(screensaver,facilityConfig);
             }
         }
 
@@ -117,24 +173,24 @@ namespace DGScope
             var principal = new WindowsPrincipal(identity);
             return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
-        static RadarWindow TryLoad(string settingsPath)
+        static RadarWindow TryLoad(string facilityConfig)
         {
             try
             {
-                return XmlSerializer<RadarWindow>.DeserializeFromFile(settingsPath);
+                return XmlSerializer<RadarWindow>.DeserializeFromFile(facilityConfig);
             }
             catch (Exception ex)
             {
                 RadarWindow radarWindow = new RadarWindow();
                 Console.WriteLine(ex.StackTrace);
-                var mboxresult = MessageBox.Show("Error reading settings file "+ settingsPath + "\n" + 
+                var mboxresult = MessageBox.Show("Error reading settings file "+ facilityConfig + "\n" + 
                     ex.Message + "\nPress Abort to exit, Retry to try again, or Ignore to destroy " +
                     "the file and start a new config.", "Error reading settings file", 
                     MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
                 if (mboxresult == DialogResult.Abort)
                     Environment.Exit(1);
                 else if (mboxresult == DialogResult.Retry)
-                    return TryLoad(settingsPath);
+                    return TryLoad(facilityConfig);
                 else
                 {
                     radarWindow = new RadarWindow();
