16aeb45e41b29b83cbd9c77fdb803dc363d94158
dg <dennis.graiani@gmail.com>
2022-10-02 22:56:06 -0400

    And thus from this rainy day, another wonderful feature is born unto dstars

---
+++ scope/ATPAVolume.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGScope
+{
+    public class ATPAVolume : Polygon
+    {
+        public GeoPoint MergePoint { get; set; }
+        public double MinimumSeparation { get; set; } = 3;
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int MaxAltitude { get; set; }
+        public int MinAltitude { get; set; }
+        public string? Runway { get; set; }
+        public string? Destination { get; set; }
+        public int? LDRDirection { get; set; }
+
+        private List<Aircraft> order;
+        private object orderlock = new object();
+        public bool IsInside (Aircraft aircraft)
+        {
+            if (aircraft == null)
+                return false;
+            if (aircraft.SweptLocation == null)
+                return false;
+            if (!aircraft.SweptLocation.IsInsidePolygon(this))
+                return false;
+            if (aircraft.TrueAltitude > MaxAltitude || aircraft.TrueAltitude < MinAltitude)
+                return false;
+            //if (Runway != null && Runway != aircraft.Runway)
+            //    return false;
+            if (Destination != null && Destination != aircraft.Destination)
+                return false;
+            if (LDRDirection != null && LDRDirection != (int?)aircraft.LDRDirection)
+                return false;
+            double currentdistance = aircraft.SweptLocation.DistanceTo(MergePoint);
+            double testdistance = aircraft.SweptLocation.FromPoint(aircraft.GroundSpeed / 3600d, aircraft.ExtrapolateTrack()).DistanceTo(MergePoint);
+            if (testdistance >= currentdistance) // Plane is moving away from merge point
+                return false;
+            return true;
+        }
+
+        public void CalculateATPA(List<Aircraft> aircraft, SeparationTable separationtable)
+        {
+            lock (orderlock) 
+            {
+                order = (aircraft.ToList().Where(x => IsInside(x)).OrderBy(x => x.SweptLocation.DistanceTo(MergePoint))).ToList();
+                if (order.Count > 1)
+                {
+                    order[0].ATPAFollowing = null;
+                    order[0].ATPAMileageNow = null;
+                    order[0].ATPAMileage24 = null;
+                    order[0].ATPAMileage45 = null;
+                    order[0].ATPARequiredMileage = null;
+                    order[0].ATPAStatus = null;
+                    order[0].ATPACone = null;
+                    for (int i = 1; i < order.Count; i++)
+                    {
+                        var leader = order[i - 1];
+                        var follower = order[i];
+                        follower.ATPAFollowing = leader;
+                        follower.ATPAMileageNow = follower.SweptLocation.DistanceTo(leader.SweptLocation);
+                        follower.ATPAMileage24 = follower.SweptLocation.FromPoint(follower.GroundSpeed * 24 / 3600d, follower.ExtrapolateTrack())
+                            .DistanceTo(leader.SweptLocation.FromPoint(leader.GroundSpeed * 24 / 3600d, leader.ExtrapolateTrack()));
+                        follower.ATPAMileage45 = follower.SweptLocation.FromPoint(follower.GroundSpeed * 45 / 3600d, follower.ExtrapolateTrack())
+                            .DistanceTo(leader.SweptLocation.FromPoint(leader.GroundSpeed * 45 / 3600d, leader.ExtrapolateTrack()));
+                        if (follower.ATPAMileageNow < MinimumSeparation || follower.ATPAMileage24 < MinimumSeparation)
+                            follower.ATPAStatus = ATPAStatus.Alert;
+                        else if (follower.ATPAMileage45 < MinimumSeparation)
+                            follower.ATPAStatus = ATPAStatus.Caution;
+                        else
+                            follower.ATPAStatus = ATPAStatus.Monitor;
+                        if (separationtable.TryGetValue(follower.Category, out SerializableDictionary<string, double> leaderTable))
+                        {
+                            if (leaderTable.TryGetValue(leader.Category, out double miles))
+                                follower.ATPARequiredMileage = miles;
+                            else
+                                follower.ATPARequiredMileage = MinimumSeparation;
+                        }
+                        else
+                        {
+                            follower.ATPARequiredMileage = MinimumSeparation;
+                        }
+                        follower.ATPATrackToLeader = follower.SweptLocation.BearingTo(leader.SweptLocation);
+                    }
+                }
+                else if (order.Count == 1)
+                {
+                    order[0].ATPAFollowing = null;
+                    order[0].ATPAMileageNow = null;
+                    order[0].ATPAMileage24 = null;
+                    order[0].ATPAMileage45 = null;
+                    order[0].ATPARequiredMileage = null;
+                    order[0].ATPAStatus = null;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    
+}
---
+++ scope/ATPAVolumeSelector.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DGScope
+{
+    partial class ATPAVolumeSelector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(456, 450);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // VideoMapSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.Name = "VideoMapSelector";
+            this.Text = "VideoMapSelector";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
