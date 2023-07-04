da8f2564295fd9c80a8455a8ad984ef73aea87a4
Dennis <dennis.graiani@gmail.com>
2023-07-04 13:27:21 -0400

    fix blurriness from rotating about a million times

--- scope/RadarWindow.cs
+++ scope/RadarWindow.cs
@@ -3546,10 +3546,6 @@ namespace DGScope
 
         private void DrawPolygon (Polygon polygon)
         {
-            GL.Rotate(ScreenRotation, 0.0f, 0.0f, 1.0f);
-            GL.PushMatrix();
-            GL.Scale((60d / scale) * Math.Cos(ScreenCenterPoint.Latitude * (Math.PI/180)), (60d / scale)  , 1.0);
-            GL.Translate(-ScreenCenterPoint.Longitude, -ScreenCenterPoint.Latitude, 0.0f);
             //GL.Scale(aspect_ratio, 1.0f, 1.0f);
             GL.Begin(PrimitiveType.Polygon);
             GL.Color4(polygon.Color);
@@ -3571,8 +3567,6 @@ namespace DGScope
                 GL.End();
                 GL.Disable(EnableCap.PolygonStipple);
             }
-            GL.PopMatrix();
-            GL.Rotate(-ScreenRotation, 0.0f, 0.0f, 1.0f);
         }
 
         private void DrawNexrad()
@@ -3586,11 +3580,19 @@ namespace DGScope
             }
         
             var polygons = Nexrad.Polygons(ScreenCenterPoint, scale, ScreenRotation);
+            GL.Rotate(ScreenRotation, 0.0f, 0.0f, 1.0f);
+            GL.PushMatrix();
+            GL.Scale((60d / scale) * Math.Cos(ScreenCenterPoint.Latitude * (Math.PI / 180)), (60d / scale), 1.0);
+            GL.Translate(-ScreenCenterPoint.Longitude, -ScreenCenterPoint.Latitude, 0.0f);
+
             for (int i = 0; i < polygons.Length; i++)
             {
                 if(polygons[i].Color.A > 0)
                     DrawPolygon(polygons[i]);
             }
+
+            GL.PopMatrix();
+            GL.Rotate(-ScreenRotation, 0.0f, 0.0f, 1.0f);
             //}
         }
         private void DrawLine (float x1, float y1, float x2, float y2, Color color, float width = 1)
