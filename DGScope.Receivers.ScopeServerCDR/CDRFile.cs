27e29c160e74da1a559506d0124b67ed850f9dfb
Dennis <dennis.graiani@gmail.com>
2024-07-07 17:32:37 -0400

    initial create of cdr file playback

---
+++ DGScope.Receivers.ScopeServerCDR/CDRFile.cs
@@ -0,0 +1,94 @@
+﻿using DGScope.Library;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace DGScope.Receivers.Falcon
+{
+    internal class CDRFile
+    {
+        public List<Update> Updates { get; } = new List<Update>();
+        public List<string> Sites { get; } = new List<string>();
+
+        public DateTime StartOfData
+        {
+            get
+            {
+                return Updates.First().TimeStamp;
+            }
+        }
+        public DateTime EndOfData
+        {
+            get
+            {
+                return Updates.Last().TimeStamp;
+            }
+        }
+
+        public TimeSpan LengthOfData
+        {
+            get
+            {
+                return EndOfData - StartOfData;
+            }
+        }
+        public CDRFile() { }
+        public static CDRFile FromFile(string filepath)
+        {
+            CDRFile newFile = new CDRFile();
+            using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                using (var zipstream = new GZipStream(stream, CompressionMode.Decompress, false))
+                {
+                    uint length = 0;
+                    do
+                    {
+                        length = getLength(zipstream);
+                        if (length > 0)
+                        {
+                            try
+                            {
+                                newFile.Updates.Add(getUpdate(zipstream, length));
+                            }
+                            catch { }
+                        }
+                    } while (length > 0);
+                }
+            }
+            return newFile;
+        }
+        private static Update getUpdate (Stream stream, uint length)
+        {
+            var bytes = new byte[length];
+            stream.Read(bytes, 0, (int)length);
+            Update update = null;
+            using (var ms = new MemoryStream(bytes))
+            {
+                update = Serializer.Deserialize<Update>(ms);
+            }
+            return update;
+        }
+        private static uint getLength (Stream stream)
+        {
+            bool lengthread = false;
+            uint length = 0;
+            for (int i = 0; !lengthread && i < 5; i++)
+            {
+                int data = stream.ReadByte();
+                if (data < 0)
+                {
+                    return 0;
+                }
+                else if (data < 0x80)
+                {
+                    lengthread = true;
+                }
+                length += (uint)((data & 0x7f) << (i * 7));
+            }
+            return length;
+        }
+    }
+}
---
+++ DGScope.Receivers.ScopeServerCDR/ScopeServerCDRReceiver.cs
@@ -0,0 +1,181 @@
+﻿using DGScope.Library;
+using DGScope.Receivers.ScopeServer;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace DGScope.Receivers.Falcon
+{
+    public class ScopeServerCDRReceiver : Receiver
+    {
+        private PlaybackControlForm PlaybackForm;
+        private CDRFile file = null;
+        private Dictionary<int, Aircraft> trackDictionary = new Dictionary<int, Aircraft>();
+        private ScopeServerClient client = new ScopeServerClient()
+        {
+            Enabled = false
+        };
+
+        private DateTime lastUpdate;
+        private TimeSpan manualAdjust = TimeSpan.Zero;
+        private Stopwatch stopwatch = new Stopwatch();
+        private Timer timer;
+
+        public bool IncludeUncorrelated { get; set; } = false;
+        internal double Speed { get; set; } = 1.0d;
+        internal DateTime CurrentTime
+        {
+            get => lastUpdate + TimeSpan.FromMilliseconds(stopwatch.Elapsed.TotalMilliseconds * Speed) + manualAdjust;
+            set
+            {
+                manualAdjust = value - CurrentTime;
+                lock (aircraft)
+                {
+                    lock (trackDictionary)
+                    {
+                        aircraft.ToList().ForEach(x => aircraft.Remove(x));
+                        //aircraft.Clear();
+                        trackDictionary.Clear();
+                    }
+                }
+            }
+        }
+
+        internal List<string> Sites { get; set; } = new List<string>();
+        internal string SelectedSite { get; set; } = string.Empty;
+
+        internal bool Playing = false;
+        internal DateTime? StartOfData
+        {
+            get
+            {
+                if (File == null)
+                {
+                    return null;
+                }
+                return File.StartOfData;
+            }
+        }
+        internal DateTime? EndOfData
+        {
+            get
+            {
+                if (File == null)
+                {
+                    return null;
+                }
+                return File.EndOfData;
+            }
+        }
+        internal TimeSpan? LengthOFData
+        {
+            get
+            {
+                if (File == null)
+                {
+                    return null;
+                }
+                return File.LengthOfData;
+            }
+        }
+        internal CDRFile File
+        {
+            get
+            {
+                return file;
+            }
+            set
+            {
+                file = value;
+                if (file != null)
+                {
+                    file.Updates.Sort((x, y) => DateTime.Compare(x.TimeStamp, y.TimeStamp));
+                    stopwatch.Reset();
+                    lastUpdate = StartOfData.Value;
+                    Sites = file.Sites;
+                    aircraft.Clear();
+                }
+            }
+        }
+
+        internal void Play()
+        {
+            if (timer == null)
+            {
+                timer = new Timer(timerCallback, null, 100, 100);
+            }
+            else
+            {
+                timer.Change(10, 10);
+            }
+            stopwatch.Start();
+            Playing = true;
+        }
+        internal void Pause()
+        {
+            //timer.Change(Timeout.Infinite, Timeout.Infinite);
+            stopwatch.Stop();
+            Playing = false;
+        }
+
+        private void timerCallback(object state)
+        {
+            RadarWindow.CurrentTime = CurrentTime;
+            var updates = file.Updates.Where(x => x.TimeStamp > lastUpdate && x.TimeStamp <= CurrentTime);
+            updates.ToList().ForEach(x => sendUpdate(x));
+            if (Playing)
+            {
+                lastUpdate = CurrentTime;
+                manualAdjust = TimeSpan.Zero;
+                stopwatch.Restart();
+            }
+            PlaybackForm.UpdateCallback();
+        }
+
+        private void sendUpdate(Update update)
+        {
+            client.ProcessUpdate(update);
+        }
+        public override void Start()
+        {
+            client.SetAircraftList(this.aircraft);
+            if (PlaybackForm == null)
+            {
+                PlaybackForm = new PlaybackControlForm(this);
+            }
+            if (!PlaybackForm.Visible)
+            {
+                PlaybackForm.Show();
+            }
+        }
+
+        public override void Stop()
+        {
+            if (PlaybackForm != null)
+            {
+                PlaybackForm.Hide();
+            }
+        }
+
+        private Aircraft GetPlane(int trackID)
+        {
+            Aircraft plane;
+            lock (trackDictionary)
+            {
+                if (!trackDictionary.TryGetValue(trackID, out plane))
+                {
+                    plane = GetNewPlane(trackID);
+                }
+            }
+            return plane;
+        }
+        private Aircraft GetNewPlane(int trackID)
+        {
+            Aircraft plane = GetPlane(Guid.NewGuid(), true);
+            trackDictionary.Add(trackID, plane);
+            return plane;
+        }
+    }
+}
